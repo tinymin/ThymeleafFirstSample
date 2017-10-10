6ff54de4a003356317c47ee2ae808059a56b8fed
tinymin <tinymin@yahoo.co.kr>
2017-10-10 21:43:55 +0900

    Thymeleaf 샘플 첫 등록

---
+++ src/main/java/com/tinymin/thymeleaftest/ThymeleaftestApplication.java
@@ -0,0 +1,12 @@
+package com.tinymin.thymeleaftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tinymin/thymeleaftest/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.tinymin.thymeleaftest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+    @Value("${welcome2.message:test}")
+    private String msg = "Hello World";
+
+    @RequestMapping
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.msg);
+        return "welcome2";
+    }
+}
